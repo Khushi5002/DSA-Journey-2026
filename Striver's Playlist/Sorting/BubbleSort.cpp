27ea3fa6545c3637bde7f78f52a8eb127d2d9301
Khushi <khushi481521@gmail.com>
2026-01-21 14:47:26 +0530

    Optimized Bubble sort

--- Striver's Playlist/Sorting/BubbleSort.cpp
+++ Striver's Playlist/Sorting/BubbleSort.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 void bubble_sort(vector<int>&arr,int n){
     for(int i=n-1;i>=1;i--){
+        int didswap=0;
         for(int j=0; j<i; j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
             }
+        }
+        if(didswap==0){
+            break;
         }
     }
 }
