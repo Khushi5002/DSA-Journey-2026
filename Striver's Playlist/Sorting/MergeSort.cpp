850cb891cefa86a26b6d2a644154f7e16e66feee
Khushi <khushi481521@gmail.com>
2026-01-22 19:43:34 +0530

    Changed Code

---
+++ Striver's Playlist/Sorting/MergeSort.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int arr[],)
+int main(){
+    int n; cin>>n;
+    vector<int>a(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+}
